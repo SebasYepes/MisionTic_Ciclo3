935390e212bf8cde4a0419892df5d29b415149c7
Sebas Yepes <ingyepesc@gmail.com>
2022-09-14 13:25:48 -0500

    Html template de Ver empresas y servicio en el controller

--- src/main/java/com/Udea/Ciclo3/controller/ControllerFull.java
+++ src/main/java/com/Udea/Ciclo3/controller/ControllerFull.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -15,11 +16,11 @@ public class ControllerFull {
     @Autowired
     EmpresaService empresaService;
 
-    @GetMapping ({"/Empresa","/verEmpresa"})
+    @GetMapping ({"/","/verEmpresa"})
     public String viewEmpresas(Model model){
         List<Empresa> listaEmpresas=empresaService.getAllEmpresas();
         model.addAttribute("emplist",listaEmpresas);
-        return "verEmpresa";
+        return "verEmpresa"; //Llamamos al HTML
     }
 
 
